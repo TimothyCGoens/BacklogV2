8d78fe140e16ebabdd0ee743d87accadcf143de3
Tim Goens <timcgoens@gmail.com>
2020-09-13 18:36:14 -0500

    still working on the homepage

--- Client/src/Components/Home.js
+++ Client/src/Components/Home.js
@@ -7,6 +7,9 @@ const Home = () => {
       <h1>Welcome to the Backlog</h1>
       <h2>Search for your favorite games!</h2>
       <p>Add them to your Wishlist or straight to your backlog.</p>
+      <p>
+        When you are done, check the game off your list and get to the next one!
+      </p>
     </Container>
   );
 };
